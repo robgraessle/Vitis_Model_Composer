1f88368c160dbc97451a53c253172b86a8c3be05
Rob Graessle <rob.graessle@xilinx.com>
2024-10-30 12:19:19 -0400

    Revert to graph block implementation of DFT

--- Examples/AIENGINE_plus_PL/AIE_HLS/Channelizer/init.m
+++ Examples/AIENGINE_plus_PL/AIE_HLS/Channelizer/init.m
@@ -1,5 +1,6 @@
 %% Basic parameters
-N = 512; 
+N = 1; 
+NSAMP = 8192;
 Ts = 16*(7/4)/8.75e9;
 ch_filt_coef = rcosdesign(0.22,4,28,'normal');
 ch_filt_coef = 28 * ch_filt_coef / sum(ch_filt_coef(:));
